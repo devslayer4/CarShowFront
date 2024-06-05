81e3a6a250a7236fe57fe68bc8883c45e9ff476e
Andriamahaly Loiqua <hei.loiqua@gmail.com>
2024-06-05 09:38:33 +0300

    feat:add navbar

---
+++ Carproject/app/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from 'next/app';
+import RootLayout from '../layout';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
+}
+
+export default MyApp;
